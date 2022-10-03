8cc759db1f360e7eff0647f8a3c72cc771d68ca7
Tariux <tariux@protonmail.com>
2022-10-03 16:08:30 +0330

    base files and basic html/css added , thinking...

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fa">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="//fdn.fontcdn.ir"><link rel="preconnect" href="//v1.fontapi.ir"><link href="https://v1.fontapi.ir/css/Vazir" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/main.scss">
+
+    <title>Manam Debug</title>
+</head>
+
+<body>
+    <?php
+
+    include_once("./template/simpleAuthButton.php");
+    ?>
+</body>
+
+</html>
---
+++ template/simpleAuthButton.php
@@ -0,0 +1,4 @@
+<div class="manamSimpleAuth">
+    <p>من ربات نیستم</p>
+    <button>منم!</button>
+</div>
